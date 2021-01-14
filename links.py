acd8dfa5ba05bd27c07decd68f73d1501c2516e1
arwhitus <42082901+arwhitus@users.noreply.github.com>
2021-01-13 23:45:17 -0500

    first publish

---
+++ links.py
@@ -0,0 +1,28 @@
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('input.xml')
+root = tree.getroot()
+
+file = open("output.md", "w+")
+
+count = 0
+for project in root.findall('project'):
+    shortLink = project.get('href')
+    
+    fullLink = 'http://www.espaces-transfrontaliers.org' + shortLink
+    
+    title = shortLink[:-1]
+    title = title[title.rfind('/')+1:]
+    
+    line = '[' + title + '](' + fullLink + ')'
+    
+    file.write(line)
+    file.write('\n')
+    
+    print(line)
+    count = count + 1
+    
+file.close()
+
+message = 'Links saved: ' + str(count)
+print(message)
